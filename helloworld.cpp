496edc9aa576b7d27d0c6cf7d5d3fec1c501671c
ruisnow <ruisnow@msn.com>
2020-05-17 11:22:27 +0800

    修改helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
-
-using namespace std;
+#include <fstream>
+#include<cstring>
 
 int  main(int argc, char *argv[]){
-    cout<<"hello world"<<endl;
+
+    using namespace std;
+    string st("Hello World!");
+
+    cout << "hello world" << endl;
+    cout << st << endl;   
+
+
+    ifstream fin;
+    fin.open("tobuy.txt");
+    if(fin.is_open() == false)
+    {
+        cerr << "Can't open file. Bye.\n";
+        exit(EXIT_FAILURE);
+    }
+    string item;
+    int count = 0;
+    getline(fin, item, ':');
+    while(fin)
+    {
+        ++count;
+        cout << count << ": " << item << endl;
+        getline(fin, item, ':');
+    }
+
+    cout << "Done\n";
+    fin.close();
+
+
+
+
+
     return 0;
 }
